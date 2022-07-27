fa4d2ee63d3e9b393bfbbaac88bd96993d3f4107
prashanttpurii <iamprashantpuri@gmail.com>
2022-07-27 07:58:13 +0530

    updated button link for heroku

--- index.php
+++ index.php
@@ -29,8 +29,7 @@
     <button type="button" class="btn btn-primary">Click Me</button>
     <button type="button" class="btn btn-success">Click Me</button>
     <button type="button" class="btn btn-warning">Click Me</button>
-    <a href="https://www.heroku.com" class="btn btn-danger" >Heroku</a>
-
+    <a href="https://www.heroku.com" class="btn btn-danger" target="_blank">Heroku</a>
     <br>
 
 <?php 
